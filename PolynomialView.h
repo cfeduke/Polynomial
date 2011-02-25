d1501376aa6a9f29497c8274e5e5f9e8ffb76211
Charles Feduke <charles.feduke@gmail.com>
2011-02-25 16:46:34 -0500

    Polynomial view

---
+++ PolynomialView.h
@@ -0,0 +1,19 @@
+//
+//  PolynomialView.h
+//  Polynomial
+//
+//  Created by Charles Feduke on 2/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface PolynomialView : NSView {
+	NSMutableArray *polynomials;
+}
+
+-(IBAction)createNewPolynomial:(id)sender;
+-(IBAction)deleteRandomPolynomial:(id)sender;
+
+@end
---
+++ PolynomialView.m
@@ -0,0 +1,55 @@
+//
+//  PolynomialView.m
+//  Polynomial
+//
+//  Created by Charles Feduke on 2/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PolynomialView.h"
+#import "Polynomial.h"
+#import <QuartzCore/QuartzCore.h>
+
+
+@implementation PolynomialView
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [[NSMutableArray alloc] init];
+    }
+    return self;
+}
+
+-(IBAction)createNewPolynomial:(id)sender {
+	Polynomial *p = [[Polynomial alloc] init];
+	[polynomials addObject:p];
+	[self setNeedsDisplay:YES];
+}
+
+-(IBAction)deleteRandomPolynomial:(id)sender {
+	if ([polynomials count] == 0)
+	{
+		NSBeep();
+		return;
+	}
+	
+	int i = random() % [polynomials count];
+	[polynomials removeObjectAtIndex:i];
+	[self setNeedsDisplay:YES];
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+	NSRect bounds = [self bounds];
+	[[NSColor whiteColor] set];
+	[NSBezierPath fillRect:bounds];
+	CGContextRef ctx = [[NSGraphicsContext currentContext] graphicsPort];
+	
+	CGRect cgBounds = *(CGRect *)&bounds;
+	
+	for (Polynomial *p in polynomials) {
+		[p drawInRect:cgBounds inContext:ctx];
+	}
+}
+
+@end
